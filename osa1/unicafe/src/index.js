e54a3ff1c13e13290da0f30785c6870076dced8d
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-01-28 23:43:05 +0200

    exercise 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,76 +7,81 @@ class App extends React.Component {
     this.state = {
       good: 0,
       neutral: 0,
-      bad: 0
+      bad: 0,
+      all: 0
     }
   }
 
   handlePositive = () => {
     this.setState({
-      good: this.state.good + 1
+      good: this.state.good + 1,
+      all: this.state.all + 1
     })
   }
 
   handleNeutral = () => {
     this.setState({
-      neutral: this.state.neutral + 1
+      neutral: this.state.neutral + 1,
+      all: this.state.all + 1
     })
   }
 
   handleNegative = () => {
     this.setState({
-      bad: this.state.bad + 1
+      bad: this.state.bad + 1,
+      all: this.state.all + 1
     })
+  }
+
+  countMean = () => {
+    if (this.state.all > 0) {
+      return (this.state.good - this.state.bad) / this.state.all
+    }
+    return 0
+  }
+
+  countPositives = () => {
+    if (this.state.all > 0) {
+      return this.state.good / this.state.all * 100
+    }
+    return 0
   }
 
   render() {
     return (
       <div>
         <h2>anna palautetta</h2>
-        <Button
-          handleClick={this.handlePositive}
-          text="hyvä"
-        />
-        <Button
-          handleClick={this.handleNeutral}
-          text="neutraali"
-        />
-        <Button
-          handleClick={this.handleNegative}
-          text="huono"
-        />
+        <Button handleClick={this.handlePositive} text="hyvä" />
+        <Button handleClick={this.handleNeutral} text="neutraali" />
+        <Button handleClick={this.handleNegative} text="huono" />
         <h2>statistiikka</h2>
-        <Statistics reviews={this.state} />
+        <Statistics state={this.state} mean={this.countMean()} positives={this.countPositives()} />
       </div>
     )
   }
 
 }
 
-const Statistics = ({ reviews }) => {
-  const all = reviews.good + reviews.neutral + reviews.bad
-  const mean = () => {
-    if (all === 0) { return 0 }
-    return round((reviews.good - reviews.bad) / all, 1)
-  }
-  const positives = () => {
-    if (all === 0) { return 0 }
-    return round(reviews.good / all * 100, 1)
-  }
+const Statistics = ({ state, mean, positives }) => {
   const round = (value, decimals) => {
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
   }
-
+  mean = round(mean, 1)
+  positives = round(positives, 1) + "%"
   return (
     <div>
-      <div>hyvä {reviews.good}</div>
-      <div>neutraali {reviews.neutral}</div>
-      <div>huono {reviews.bad}</div>
-      <div>keskiarvo {mean()}</div>
-      <div>positiivisia {positives()} %</div>
+      <Statistic text={"hyvä"} value={state.good} />
+      <Statistic text={"neutraali"} value={state.neutral} />
+      <Statistic text={"huono"} value={state.bad} />
+      <Statistic text={"keskiarvo"} value={mean} />
+      <Statistic text={"positiivisia"} value={positives} />
     </div>
   )
 }
+
+const Statistic = ({ text, value }) => (
+  <div>{text} {value}</div>
+)
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
