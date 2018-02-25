ef4387527ed2abbda9cba7480cf62c6241e234ab
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-02-25 19:10:33 +0200

    exercise 4.14

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -51,4 +51,23 @@ blogsRouter.delete('/:id', async (request, response) => {
   }
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  try {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(Blog.format(updatedBlog))
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malformatted id' })
+  }
+})
+
 module.exports = blogsRouter
