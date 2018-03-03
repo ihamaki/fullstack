2c4b6573a0b9917f1df29b2b6addee24dc494b3f
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-03-03 12:38:37 +0200

    exercise 5.11

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Blog extends React.Component {
+  static propTypes = {
+    blog: PropTypes.object.isRequired,
+    onBlogLike: PropTypes.func.isRequired,
+    onBlogDelete: PropTypes.func.isRequired
+  }
+
   constructor(props) {
     super(props)
     this.state = {
