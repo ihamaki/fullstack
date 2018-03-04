d1677b8fee4dee1219e60c6a16385bae9ff6233d
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-03-04 11:07:16 +0200

    exercise 5.13

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -18,4 +18,23 @@ describe('simpleblog tests', () => {
     expect(blogInfoDiv.text()).toContain(blog.author)
     expect(blogLikesDiv.text()).toContain('blog has', blog.likes, 'likes')
   })
+
+  it('clicking like button twice calls event handler twice', () => {
+    const blog = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 4
+    }
+
+    const mockHandler = jest.fn()
+    const blogComponent = shallow(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
